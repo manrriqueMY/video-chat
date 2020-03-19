3616afad61201de8f29bb7694c5b5445cccab22c
manrriqueMY <manrrique.my@gmail.com>
2020-03-18 19:37:34 -0500

    yj

--- server.js
+++ server.js
@@ -1,22 +1,52 @@
 const express = require('express');
-const app = express();
-
-const POST = process.env.POST || 3000;
+const socketio = require('socket.io');
 const http = require('http');
+const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
+const PORT = process.env.PORT || 3000;
 
-const io = require('socket.io')(server);
-const RoomService = require('./RoomService')(io);
+let _io;
+const MAX_CLIENTS = 3;
 
-io.sockets.on('connection', RoomService.listen);
-io.sockets.on('error', e => console.log(e));
+io.on('connection', function(socket){
+  const rooms = io.nsps['/'].adapter.rooms;
+  socket.on('join', function(room) {
+    let numClients = 0;
+    if (rooms[room]) {
+      numClients = rooms[room].length;
+    }
+    if (numClients < MAX_CLIENTS) {
+      socket.on('ready', function() {
+        socket.broadcast.to(room).emit('ready', socket.id);
+      });
+      socket.on('offer', function (id, message) {
+        socket.to(id).emit('offer', socket.id, message);
+      });
+      socket.on('answer', function (id, message) {
+        socket.to(id).emit('answer', socket.id, message);
+      });
+      socket.on('candidate', function (id, message) {
+        socket.to(id).emit('candidate', socket.id, message);
+      });
+      socket.on('disconnect', function() {
+        socket.broadcast.to(room).emit('bye', socket.id);
+      });
+      socket.join(room);
+    } else {
+      socket.emit('full', room);
+    }
+  });
+});
+io.on('error', e => console.log(e));
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
     res.sendFile(`${__dirname}/public/index.html`);
 });
-server.listen(POST, () => console.log(`Server is running on POST ${POST}`));
+
+server.listen(PORT,"192.168.1.9", () => console.log(`Server is running on PORT ${PORT}`));
 
 /*
 
